44c6710067725a76bf64761801ddd13cea89dd98
Keith Bingman <keith.bingman@unbounce.com>
2020-04-23 17:10:02 -0700

    move lesson-3 to lesson-4

--- lesson-4/components/App.tsx
+++ lesson-4/components/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useMouseDelta } from '../hooks';
-import { Box } from './Box';
 
 export const App = () => {
   const { dx, dy } = useMouseDelta();
@@ -10,8 +9,6 @@ export const App = () => {
       <h1>Lesson 3</h1>
       <p>dx: {dx}</p>
       <p>dy: {dy}</p>
-      <Box />
-      <Box />
     </div>
   );
 };
